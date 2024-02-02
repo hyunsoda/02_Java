46af1c523459ad62b115f9b7e86b4c444e5f8f1a
hyunsoda <so0144@naver.com>
2024-02-02 17:37:46 +0900

    fix

--- 02_operator/src/edu/kh/op/practice/OperatorPractice2.java
+++ 02_operator/src/edu/kh/op/practice/OperatorPractice2.java
@@ -15,7 +15,23 @@ public class OperatorPractice2 {
 		System.out.println((age >= 13) && (age <=19)? "true" :"false" );
 		
 		System.out.print("노인이거나 어린이 입니까? ");
-		System.out.println((age>= 65) && (age<=12)? "true" : "false" );
+		System.out.println((age>= 65) || (age<=12)? "true" : "false" );
 	}
 	
-}
+	public void practice5() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("당신의 나이는? ");
+		int age = sc.nextInt();
+		
+		String personType = (age>=20)? "성인" : "미성년";
+		System.out.println("저는" + personType + "입니다");
+		
+		boolean isTeenager = (age>=13) && (age<=19);
+		System.out.println("청소년입니까?" + isTeenager);
+		
+		boolean isSeniorOrChild = (age >= 65) || (age <=12);
+		System.out.println("노인이거나 어린이 입니까?" + isSeniorOrChild);
+	}
+	
+}	
