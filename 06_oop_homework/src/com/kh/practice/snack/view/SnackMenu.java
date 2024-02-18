ca0c253d0583522da2b6be60d86c3629a38b9fb0
hyunsoda <so0144@naver.com>
2024-02-18 14:34:41 +0900

    fix

--- 06_oop_homework/src/com/kh/practice/snack/view/SnackMenu.java
+++ 06_oop_homework/src/com/kh/practice/snack/view/SnackMenu.java
@@ -1,5 +1,13 @@
 package com.kh.practice.snack.view;
 
-public class SnackMenu {
+import java.util.Scanner;
 
+import com.kh.practice.snack.controller.SnackController;
+
+public class SnackMenu {
+	Scanner sc = new Scanner(System.in);
+	SnackController scr = new SnackController();
+	
+	
+	
 }
