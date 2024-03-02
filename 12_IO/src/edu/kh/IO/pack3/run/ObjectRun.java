48483d610f735c569cc71fa6b956f55f1d468df7
hyunsoda <so0144@naver.com>
2024-03-02 17:27:35 +0900

    fix

--- 12_IO/src/edu/kh/IO/pack3/run/ObjectRun.java
+++ 12_IO/src/edu/kh/IO/pack3/run/ObjectRun.java
@@ -6,7 +6,7 @@ public class ObjectRun {
 
 	public static void main(String[] args) {
 		ObjectService service = new ObjectService();
-//		service.objectOutput();
+		service.objectOutput();
 		// [직렬화가 되어있지 않다]
 		//java.io.NotSerializableException 
 		service.objectInput();
